9b52d9d6b853b6cb8e0b257d4bf46051307b5248
Guillaume Binet <gbin@gootz.net>
2015-08-12 11:14:44 -0700

    lint

--- asciiart.py
+++ asciiart.py
@@ -3,9 +3,7 @@ from pyfiglet import Figlet
 
 class AsciiArt(BotPlugin):
     """ Ascii Art related commands. """
-
     @botcmd
     def big(self, mess, args):
         """ Generates a big version of what you want to say."""
         return "```\n" + Figlet(font='slant').renderText(args) + "\n```"
-
