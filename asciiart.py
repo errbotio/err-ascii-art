653c5e44d998b9ea163a829bc7eb3d202042bdd6
Sijis Aviles <sijis.aviles+github@gmail.com>
2021-08-15 22:38:14 -0500

    style: format with black

--- asciiart.py
+++ asciiart.py
@@ -1,9 +1,11 @@
 from errbot import BotPlugin, botcmd
 from pyfiglet import Figlet
 
+
 class AsciiArt(BotPlugin):
-    """ Ascii Art related commands. """
+    """Ascii Art related commands."""
+
     @botcmd
     def big(self, mess, args):
-        """ Generates a big version of what you want to say."""
-        return "```\n" + Figlet(font='slant').renderText(args) + "\n```"
+        """Generates a big version of what you want to say."""
+        return "```\n" + Figlet(font="slant").renderText(args) + "\n```"
